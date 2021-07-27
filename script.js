f4b683fb46ef0489b2a688555a75d9fdbada9bde
sanjayyadavgithub <48355724+sanjayyadavgithub@users.noreply.github.com>
2021-07-27 22:28:50 +0530

    todolisthtmlcssjs

---
+++ script.js
@@ -0,0 +1,39 @@
+let addTodoButton = document.querySelector('.add-todo');
+let todoInput = document.querySelector('.todo-input');
+let todosList = document.querySelector('.todos-list-container');
+
+todoInput.addEventListener('keypress', function (e) {
+  if (e.key == 'Enter') {
+    addTodo();
+  }
+});
+addTodoButton.addEventListener('click', function () {
+  addTodo();
+});
+
+function addTodo() {
+  let todoInputValue = todoInput.value;
+  if (todoInputValue) {
+    appendTodo(todoInputValue);
+    todoInput.value = '';
+  }
+}
+
+function appendTodo(todo) {
+  let todoItemDiv = document.createElement('div');
+  todoItemDiv.classList.add('todo-item');
+  let pTag = document.createElement('p');
+  pTag.classList.add('todo');
+  pTag.textContent = todo;
+  let deleteTodoButton = document.createElement('button');
+  deleteTodoButton.classList.add('delete-todo');
+  deleteTodoButton.textContent = 'Delete';
+  deleteTodoButton.addEventListener('click', deleteTodo);
+  todoItemDiv.append(pTag);
+  todoItemDiv.append(deleteTodoButton);
+  todosList.append(todoItemDiv);
+}
+
+function deleteTodo(e) {
+  e.target.parentNode.remove();
+}
